2314f9e14e735707fb69e8f63afb2587e2d1aec5
mjahanseir <mjahanseir@gmail.com>
2022-03-31 20:49:30 -0600

    basic arrays in JS

---
+++ L16_Arrays/L601_AddingElements.js
@@ -0,0 +1,14 @@
+const numbers=[3,4];
+
+//Add to end of array
+numbers.push(5,6)
+console.log(numbers)
+
+//Add to Beginning of array
+numbers.unshift(1,2)
+console.log(numbers)
+
+
+//Add to middle of array
+numbers.splice(2,0,'a','b')
+console.log(numbers)
