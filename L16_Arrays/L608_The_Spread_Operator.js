5f57f930ea8b0d102dc83006ff3ffdd85a67154b
mjahanseir <mjahanseir@gmail.com>
2022-04-02 21:44:07 -0600

    Arrays in JS

---
+++ L16_Arrays/L608_The_Spread_Operator.js
@@ -0,0 +1,10 @@
+
+const first=[1,2,3];
+const second=[4,5,6];
+
+const combined=first.concat(second);
+console.log(combined);
+
+console.log(...first, ...second); //  Spread Operator
+console.log(...first, "c",...second);
+
