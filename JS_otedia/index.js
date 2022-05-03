0a43f76887d5c7d964583820063ba57e0a45ec78
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-03 09:06:48 -0600

    Js with otedia

--- JS_otedia/index.js
+++ JS_otedia/index.js
@@ -1,22 +1,55 @@
-console.log(typeof 20);
-console.log(typeof 8.5);
+let message = "hello";
+console.log(message);
 
-let a = Number.isInteger;
-console.log(a);
+let str = "It's a car";
+console.log(str);
+str = 'My name is "Mike"';
+console.log(str);
 
-console.log(Number.isFinite(1 / 0));
-console.log(Number.isFinite(1 / 1));
-console.log(Number.isNaN(1 / 0));
-console.log(Number.isNaN(NaN));
+str = 'My name is "Mike" and It\'s my car';
+console.log(str);
 
-let b = 30000;
-console.log(b);
-console.log(b.toExponential());
+str =
+  'Hello World!\nMy name is "Mike" and It\'s my car\nMy profile folder is : C:\\Users\\Mike';
+console.log(str);
 
-let c = 23.28;
-console.log(c.toFixed(1));
+let name = "Mike",
+  age = 20;
+str = `My name is ${name}`;
+console.log(str);
+str = `I'm ${age}, and my brother is two years older. So he is ${age + 2}`;
+console.log(str);
 
-let d = 23.286756756567567;
-console.log(d.toPrecision(6));
+str = `Hello World!
+My name is ${name} and It's my car
+My profile folder is : c:\\Users\\${name}`;
+console.log(str);
 
-console.log((23.6585875865865).toPrecision(9));
+console.log(" -----   String Methods & Properties  ------- ");
+
+let st = "Hello World!";
+console.log(st.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str.charAt(1));
+console.log(str.charAt(6));
+console.log(str[6]);
+
+str = "Hello, welcome to JavaScript!";
+console.log(str.indexOf("o"));
+console.log(str.indexOf("to"));
+console.log(str.indexOf("Java"));
+console.log(str.indexOf("java"));
+
+console.log(str.indexOf("o", 5));
+console.log(str.indexOf("o", 13));
+
+console.log(str.lastIndexOf("o"));
+console.log(str.lastIndexOf("o", 10));
+console.log(str.lastIndexOf("css"));
+
+console.log(str.includes("welcome"));
+console.log(str.includes("JavaScript"));
+console.log(str.includes("javascript"));
