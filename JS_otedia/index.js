85b2bd62ea126853c22c03f20deef9ccbcca82ee
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-03 09:12:08 -0600

    Js with otedia

--- JS_otedia/index.js
+++ JS_otedia/index.js
@@ -1,34 +1,34 @@
 let message = "hello";
 console.log(message);
 
-let str = "It's a car";
-console.log(str);
-str = 'My name is "Mike"';
-console.log(str);
+let str0 = "It's a car";
+console.log(str0);
+str0 = 'My name is "Mike"';
+console.log(str0);
 
-str = 'My name is "Mike" and It\'s my car';
-console.log(str);
+str11 = 'My name is "Mike" and It\'s my car';
+console.log(str11);
 
-str =
+str1 =
   'Hello World!\nMy name is "Mike" and It\'s my car\nMy profile folder is : C:\\Users\\Mike';
-console.log(str);
+console.log(str1);
 
 let name = "Mike",
   age = 20;
-str = `My name is ${name}`;
-console.log(str);
-str = `I'm ${age}, and my brother is two years older. So he is ${age + 2}`;
-console.log(str);
+str2 = `My name is ${name}`;
+console.log(str2);
+str2 = `I'm ${age}, and my brother is two years older. So he is ${age + 2}`;
+console.log(str2);
 
-str = `Hello World!
+st = `Hello World!
 My name is ${name} and It's my car
 My profile folder is : c:\\Users\\${name}`;
-console.log(str);
+console.log(st);
 
 console.log(" -----   String Methods & Properties  ------- ");
 
-let st = "Hello World!";
-console.log(st.length);
+let str = "Hello World!";
+console.log(str.length);
 
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
@@ -46,10 +46,10 @@ console.log(str.indexOf("java"));
 console.log(str.indexOf("o", 5));
 console.log(str.indexOf("o", 13));
 
-console.log(str.lastIndexOf("o"));
-console.log(str.lastIndexOf("o", 10));
-console.log(str.lastIndexOf("css"));
+console.log('str.lastIndexOf("o") : ' + str.lastIndexOf("o"));
+console.log('str.lastIndexOf("o", 10) : ' + str.lastIndexOf("o", 10));
+console.log('str.lastIndexOf("css") : ' + str.lastIndexOf("css"));
 
-console.log(str.includes("welcome"));
+console.log('str.includes("welcome") : ' + str.includes("welcome"));
 console.log(str.includes("JavaScript"));
 console.log(str.includes("javascript"));
