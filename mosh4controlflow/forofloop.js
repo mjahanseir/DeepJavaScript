2957518b4b913ff6da70f59ac24173aef1a60db9
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-11 23:16:39 -0600

    setup objects

---
+++ mosh4controlflow/forofloop.js
@@ -0,0 +1,11 @@
+let person={
+    name:'mo',
+    age:20
+};
+for(let i in person){
+    console.log(i, person[i])
+}
+
+const color=['r','g','b'];
+for(let i in color)
+    console.log(i, color[i])
