4c5e4b649af2b806b9b3fb254c5bf9250bb7f345
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-16 23:47:16 -0600

    3. Build a Blackjack game second part done

--- LearnJavaScript/3. Build a Blackjack game/9. Let's practice boolean conditions/index.js
+++ LearnJavaScript/3. Build a Blackjack game/9. Let's practice boolean conditions/index.js
@@ -6,20 +6,20 @@ console.log(3 >= 3)   //
 console.log(11 <= 11) //
 console.log(3 <= 2)   //
 
-// let firstCard = 10
-// let secondCard = 11
-// let sum = firstCard + secondCard
-// let hasBlackJack = false
-// let isAlive = true
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
-// if (sum <= 20) {
-//     console.log("Do you want to draw a new card? 🙂")
-// } else if (sum === 21) {
-//     console.log("Wohoo! You've got Blackjack! 🥳")
-//     hasBlackJack = true
-// } else {
-//     console.log("You're out of the game! 😭")
-//     isAlive = false
-// }
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("You're out of the game! 😭")
+    isAlive = false
+}
 
-// console.log(isAlive)
+console.log(isAlive)
