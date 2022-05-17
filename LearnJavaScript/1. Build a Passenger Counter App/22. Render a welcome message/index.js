51a7e76a7d89e2178b4099203d3bcd9198df3dc1
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-16 22:04:32 -0600

    first js app from scratch

--- LearnJavaScript/1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ LearnJavaScript/1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,6 +1,9 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
 
+let welcomeEl= document.getElementById("welcome-el")
 // Create two variables (name & greeting) that contains your name
 // and the greeting we want to render on the page
-
+let name ="Mo"
+let  greeting = "Welcom to this page  "+ name;
 // Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText= greeting;
--- LearnJavaScript/1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
+++ LearnJavaScript/1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
@@ -4,8 +4,10 @@ let name = "Per Harald Borgen"
 let greeting = "Welcome back "
 
 welcomeEl.innerText = greeting + name
+welcomeEl.innerText = welcomeEl.innerText + "emoji"
 
 // Add an emoji to the end!
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
 
+let count = count + 1;
