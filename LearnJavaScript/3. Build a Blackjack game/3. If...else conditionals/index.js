2882fefe04c17cf31cc43b4ec2005c74e4e6f178
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-16 23:40:16 -0600

    3. Build a Blackjack game first done

--- LearnJavaScript/3. Build a Blackjack game/3. If...else conditionals/index.js
+++ LearnJavaScript/3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,3 +2,10 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if(sum<21){
+    console.log("new card")
+}else if(sum===21){
+    console.log("won")
+}else{
+    console.log("lose")
+}
