8fbe3ef55cb3fa71212ac353314a09225b8c584d
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-18 14:39:17 -0600

    5. Build a Chrome Extension 2ed part done

--- LearnJavaScript/5. Build a Chrome Extension/10. Push to the myLeads array/index.js
+++ LearnJavaScript/5. Build a Chrome Extension/10. Push to the myLeads array/index.js
@@ -3,8 +3,8 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
-
 inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com")
     console.log("Button clicked!")
 })
 
--- LearnJavaScript/5. Build a Chrome Extension/7. Your turn to refactor/index.js
+++ LearnJavaScript/5. Build a Chrome Extension/7. Your turn to refactor/index.js
@@ -1,9 +1,11 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-function saveLead() {
-    console.log("Button clicked!")
-}
+let btn= document.getElementById("input-btn");
+
+btn.addEventListener("click", ()=>    console.log("Button clicked!"))
+
+
 
 
 
