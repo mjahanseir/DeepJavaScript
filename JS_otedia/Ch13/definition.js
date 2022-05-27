34952f6ccc56a8589891c4acc96fa69ca36531bd
Mohammad Jahanseir <mjahanseir@gmail.com>
2022-05-27 10:29:08 -0600

    chapter 13 JS

---
+++ JS_otedia/Ch13/definition.js
@@ -0,0 +1,27 @@
+// Function Definition
+// Function Declaration
+// Function Expression
+
+
+
+function showBio(){
+	console.log("First name = Abbas");
+	console.log("Last name = Moqaddam");
+	console.log("Age = 33");
+}
+
+showBio();
+showBio();
+
+
+const showBio2 = function(){
+	console.log("First name = Abbas");
+	console.log("Last name = Moqaddam");
+	console.log("Age = 33");
+}
+
+showBio2();
+showBio2();
+
+let func1 = showBio2;
+func1();
