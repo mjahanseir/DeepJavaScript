df512b5c3ed9f203aa63d6bf0ba66d5a6859c771
Mohammad Jahanseir <48261288+mjahanseir@users.noreply.github.com>
2022-08-11 11:01:29 -0600

    setup cources JS

---
+++ [Jonas]complete-javascript-course-master/01-Fundamentals-Part-1/09-linkingScript.js
@@ -0,0 +1,3 @@
+let js = "amazing";
+if (js === "amazing") console.log("Java Script is fun");
+console.log(40 + 8 - 23 + 10);
---
+++ [Jonas]complete-javascript-course-master/01-Fundamentals-Part-1/10-ValuesAndVariables.js
@@ -0,0 +1,8 @@
+let js = "amazing";
+if (js === "amazing") console.log("Java Script is fun");
+console.log(40 + 8 - 23 + 10);
+
+let fName = "Mo";
+console.log(fName);
+console.log(fName);
+console.log(fName);
